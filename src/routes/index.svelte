3ae1899af24876edddb7f33899de4305ccb63d9e
Lizzy <lizzynibali@gmail.com>
2022-06-06 11:54:35 -0600

    add basic info about me! say hi, pretty color and font

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Hi! I'm Lizzy!</h1>
+<p>I created this site for fun and to figure out how to use custom domains, set up https, etc.</p>
+<p>So there's not much here!</p>
+<p>Visit <a href="https://github.com/lizzydev/lizzydev-pages">the repo</a> for this site.</p>
+<p>Visit <a href="https://github.com/lizzydev/book-notes">book-notes</a> for books I've read and my notes from them!</p>
+
+<style>
+    :global(body){
+        background-color: rgb(33, 175, 168);
+    }
+
+    :global(p){
+        font-family: fantasy;
+    }
+</style>
