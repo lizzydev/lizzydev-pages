044ad92eb62a286c0c828f4d2663af8b66e22f52
Lizzy <lizzynibali@gmail.com>
2022-06-06 12:26:46 -0600

    adjust styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,17 @@
-<h1>Hi! I'm Lizzy!</h1>
-<p>I created this site for fun and to figure out how to use custom domains, set up https, etc.</p>
-<p>So there's not much here!</p>
-<p>Visit <a href="https://github.com/lizzydev/lizzydev-pages">the repo</a> for this site.</p>
-<p>Visit <a href="https://github.com/lizzydev/book-notes">book-notes</a> for books I've read and my notes from them!</p>
+<body>
+    <h1>Hi! I'm Lizzy!</h1>
+    <p>I created this site for fun and to figure out how to use custom domains, set up https, etc.</p>
+    <p>So there's not much here!</p>
+    <p>Visit <a href="https://github.com/lizzydev/lizzydev-pages">the repo</a> for this site.</p>
+    <p>Visit <a href="https://github.com/lizzydev/book-notes">book-notes</a> for books I've read and my notes from them!</p>
+</body>
 
 <style>
-    :global(body){
-        background-color: rgb(33, 175, 168);
+    body {
+        background-color: rgba(247, 173, 62, 0.322);
     }
 
-    :global(p){
+    p {
         font-family: fantasy;
     }
 </style>
