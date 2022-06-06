501c6ad00f2d40b693a73bbc033cc9bc9def2516
Lizzy <lizzynibali@gmail.com>
2022-06-06 14:05:43 -0600

    remove path shenanigans

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === "development";
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess:[
@@ -15,10 +13,6 @@ const config = {
 			pages: 'docs',
 			assets: 'docs'
 		}),
-        paths: {
-            // change below to your repo name
-            base: dev ? '' : '/lizzydev-pages',
-        },
 		prerender: {
 			default: true
 		}
